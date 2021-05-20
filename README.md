0e6da4615ec531ad12b507381cc65f4c3e8aa0f8
wildzong <739177216@qq.com>
2021-05-20 14:21:53 +0800

    create project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Doraemon
-a project use angular and beego to talk with friends and manage your task
---
+++ src/test/java/com/doraemon/lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doraemon.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
