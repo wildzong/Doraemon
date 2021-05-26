d67df13809b10b8307666e1025f711202447416c
wildzong <739177216@qq.com>
2021-05-26 17:01:01 +0800

    加入token

--- src/main/java/com/doraemon/lottery/base/security/WebSecurityConfig.java
+++ src/main/java/com/doraemon/lottery/base/security/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.doraemon.lottery.base.security;
 
+import com.doraemon.lottery.base.jwt.JWTAuthenticationEntryPoint;
+import com.doraemon.lottery.base.jwt.JWTAuthenticationFilter;
+import com.doraemon.lottery.base.jwt.JWTAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -31,14 +35,61 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        // 跨域共享
+        http.cors()
+                .and()
+                // 跨域伪造请求限制无效
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/addUser").permitAll() // 允许post请求/add-user，而无需认证
-                .anyRequest().authenticated() // 所有请求都需要验证
+                .antMatchers(HttpMethod.POST, "/addUser").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin() // 使用默认的登录页面
+                // 添加JWT登录拦截器
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                // 添加JWT鉴权拦截器
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                // 设置Session的创建策略为：Spring Security永不创建HttpSession 不使用HttpSession来获取SecurityContext
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .csrf().disable();// post请求要关闭csrf验证,不然访问报错；实际开发中开启，需要前端配合传递其他参数
+                // 异常处理
+                .exceptionHandling()
+                // 匿名用户访问无权限资源时的异常
+                .authenticationEntryPoint(new JWTAuthenticationEntryPoint());
+
+//        http
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/addUser").permitAll() // 允许post请求/add-user，而无需认证
+//                .anyRequest().authenticated() // 所有请求都需要验证
+//                .and()
+//                .formLogin() // 使用默认的登录页面
+//                .and()
+//                .csrf().disable();// post请求要关闭csrf验证,不然访问报错；实际开发中开启，需要前端配合传递其他参数
+
+//        // 跨域共享
+//        http.cors()
+//                .and()
+//                // 跨域伪造请求限制无效
+//                .csrf().disable()
+//                .authorizeRequests()
+//                // 访问/data需要ADMIN角色
+//                .antMatchers("/user").hasRole("ADMIN")
+//                // 其余资源任何人都可访问
+//                .anyRequest().permitAll()
+//                .and()
+//                // 添加JWT登录拦截器
+//                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+//                // 添加JWT鉴权拦截器
+//                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+//                .sessionManagement()
+//                // 设置Session的创建策略为：Spring Security永不创建HttpSession 不使用HttpSession来获取SecurityContext
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                // 异常处理
+//                .exceptionHandling()
+//                // 匿名用户访问无权限资源时的异常
+//                .authenticationEntryPoint(new JWTAuthenticationEntryPoint());
+
     }
 
     /**
@@ -56,5 +107,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 从数据库读取的用户进行身份认证
                 .userDetailsService(myUserDetailService)
                 .passwordEncoder(passwordEncoder());
+
     }
 }
