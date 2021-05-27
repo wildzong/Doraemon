74fd62b8199276c1b3864261e71822d12cdf8423
wildzong <739177216@qq.com>
2021-05-27 19:15:46 +0800

    加入redis，未验证成功

---
+++ src/main/java/com/doraemon/lottery/base/redis/LettuceRedisConfig.java
@@ -0,0 +1,27 @@
+package com.doraemon.lottery.base.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @Author: wildzong
+ * @Description: 自定义 RedisTemplate ，设置序列化器
+ * @Date: 2021-05-27 19:10
+ */
+@Configuration
+public class LettuceRedisConfig {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
---
+++ src/test/java/com/doraemon/lottery/base/redis/LettuceRedisConfigTest.java
@@ -0,0 +1,23 @@
+package com.doraemon.lottery.base.redis;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.io.Serializable;
+
+@SpringBootTest
+public class LettuceRedisConfigTest {
+    @Autowired
+    private RedisTemplate<String, String> strRedisTemplate;
+    @Autowired
+    private RedisTemplate<String, Serializable> serializableRedisTemplate;
+
+    @Test
+    public void testString() {
+        strRedisTemplate.opsForValue().set("strKey", "zwqh");
+        System.out.println(strRedisTemplate.opsForValue().get("strKey"));
+    }
+
+}
